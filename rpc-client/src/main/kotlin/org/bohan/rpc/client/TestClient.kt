57aae8056f74adca08b4c28a57f12eb04cecb173
ybh2333 <bohan_young@qq.com>
2025-01-01 13:49:39 +0800

    fix: 修改测试客户端，使其能够支持基于 zk 的服务发现

--- rpc-client/src/main/kotlin/org/bohan/rpc/client/TestClient.kt
+++ rpc-client/src/main/kotlin/org/bohan/rpc/client/TestClient.kt
@@ -6,15 +6,17 @@ import org.bohan.rpc.client.client.impl.SimpleSocketRpcClient
 import org.bohan.rpc.client.conf.ClientConfig
 import org.bohan.rpc.client.conf.enums.ClientType
 import org.bohan.rpc.client.proxy.ClientProxy
+import org.bohan.rpc.client.registry.impl.ZkServiceCenter
 import org.bohan.rpc.contract.service.UserService
 import org.slf4j.LoggerFactory
 
 
 fun main() {
     val config = ConfigLoader.loadConfig(ClientConfig::class.java)
+    val serviceCenter = ZkServiceCenter()
     val client = when (ClientType.getEnumByName(config.clientType)) {
         ClientType.SIMPLE_RPC_CLIENT -> SimpleSocketRpcClient(config.host, config.port)
-        ClientType.NETTY_RPC_CLIENT -> NettyRpcClient(config.host, config.port)
+        ClientType.NETTY_RPC_CLIENT -> NettyRpcClient(serviceCenter)
     }
 
     val proxy = ClientProxy(client)
